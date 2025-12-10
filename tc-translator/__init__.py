b5d4709bca56cc322a7c48adf293647ab5bb06eb
Mich-Seth Owusu <michsethowusu@gmail.com>
2025-12-10 12:34:07 +0100

    Delete tc-translator directory

--- tc-translator/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-TC Translator - Terminology Controlled Translation System
-"""
-
-from .translator import Terminex, TranslationResult
-from .utils import translate
-
-__version__ = "1.0.0"
-__all__ = ["Terminex", "TranslationResult", "translate"]
