0aa4278f862cded5c4a1f51cec038755265b8b1d
Mich-Seth Owusu <michsethowusu@gmail.com>
2025-12-10 13:27:49 +0100

    Update language_codes.py

--- tc_translate/language_codes.py
+++ tc_translate/language_codes.py
@@ -6,7 +6,6 @@ Language code mapping between ISO 639-1 (2-letter) and ISO 639-3 (3-letter) code
 LANGUAGE_CODE_MAPPING = {
     # Common language mappings
     'twi': 'ak',  # Twi -> Akan (Google uses 'ak' for Akan which includes Twi)
-    'aka': 'ak',  # Akan
     'fra': 'fr',  # French
     'deu': 'de',  # German
     'spa': 'es',  # Spanish
@@ -20,7 +19,7 @@ LANGUAGE_CODE_MAPPING = {
     'ara': 'ar',  # Arabic
     'hin': 'hi',  # Hindi
     'ben': 'bn',  # Bengali
-    'urd': 'ur',  # Urdu
+    'gaa': 'gaa',  # Urdu
     'swa': 'sw',  # Swahili
     'yor': 'yo',  # Yoruba
     'ibo': 'ig',  # Igbo
@@ -32,7 +31,6 @@ LANGUAGE_CODE_MAPPING = {
     'ful': 'ff',  # Fulah
     'mand': 'man',  # Mandingo
     'ewe': 'ee',  # Ewe
-    'twi': 'tw',  # Twi (alternate)
     'tir': 'ti',  # Tigrinya
     'orm': 'om',  # Oromo
 }
@@ -83,13 +81,13 @@ def get_available_google_languages() -> list:
     """Get list of languages supported by Google Translate."""
     # Google Translate supports these major languages
     return [
-        'af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 
-        'ceb', 'zh', 'zh-cn', 'zh-tw', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 
+        'af', 'ak', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 
+        'ceb', 'zh', 'zh-cn', 'zh-tw', 'co', 'hr', 'cs', 'da', 'ee', 'en', 'eo', 
         'et', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 
         'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 
         'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 
         'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'or', 
-        'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 
+        'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gaa', 'sr', 'st', 'sn', 
         'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 
         'tt', 'te', 'th', 'tr', 'tk', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 
         'yi', 'yo', 'zu'
